3f4904188fbb170eba15c7819bba36cd290810eb
endamccormack <endamccormack0@gmail.com>
2017-10-16 14:51:19 +0100

    initial commit of template with readme, gitignore, pom and a basic project structure with classes.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Template
+This is a tiny template application for making new contexts
+
+## prerequisites
+1. Java 1.8
+2. Maven
---
+++ src/main/java/academy/dd/awp/template/Template.java
@@ -0,0 +1,7 @@
+package academy.dd.awp.template;
+
+public class Template {
+    public Template() {
+        throw new IllegalArgumentException("This should throw an exception");
+    }
+}
